fb047a8fbf8339f8a3cba532d4eb9ee4836b6aac
Federico Nicolas Liendo Molina <fnliendomolina@gmail.com>
2021-09-24 02:53:24 -0300

    Revert "Commit"
    This reverts commit acbf564fd1811f4940732cc2dc2547a8e718cf99.

---
+++ WSVentas/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSVentas.Models.Request;
+using WSVentas.Models.Response;
+using WSVentas.Services;
+
+namespace WSVentas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Autentificar([FromBody] AuthRequest model)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            var userresponse = _userService.Auth(model);
+
+            if (userresponse == null)
+            {
+                oRespuesta.Mensaje = "Usuario o contraseña incorrecta";
+                oRespuesta.Exito = 0;
+                return BadRequest();
+            }
+
+            oRespuesta.Exito = 1;
+            oRespuesta.Data = userresponse;
+            return Ok(oRespuesta);
+        }
+    }
+}
---
+++ WSVentas/Controllers/VentaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSVentas.Models;
+using WSVentas.Models.Request;
+using WSVentas.Models.Response;
+using WSVentas.Services;
+
+namespace WSVentas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class VentaController : ControllerBase
+    {
+        private IVentaService _venta;
+
+        public VentaController(IVentaService venta)
+        {
+            this._venta = venta;
+        }
+
+        [HttpPost]
+        public IActionResult Add(VentaRequest model)
+        {
+            Respuesta respuesta = new Respuesta();
+            try
+            {
+                _venta.Add(model);
+                respuesta.Exito = 1;
+            }
+            catch(Exception ex)
+            {
+                respuesta.Exito = 0;
+                respuesta.Mensaje = ex.Message;
+            }
+            return Ok(respuesta);
+        }
+    }
+}
